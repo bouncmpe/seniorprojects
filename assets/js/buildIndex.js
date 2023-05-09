b43aa084ed44cb29446a32653e9332a44e701808
nurlan <nurlandadashov2002@yahoo.com>
2023-05-09 10:37:17 +0300

    save work

---
+++ assets/js/buildIndex.js
@@ -0,0 +1,66 @@
+const CWD = process.cwd();
+const fs = require('fs');
+const lunr = require('lunr');
+require(CWD + "/assets/js/lunr.unicodeNormalizer.js")(lunr);
+
+async function initSearchIndex() {
+    try {
+        fs.readFile(CWD + "/public/index.json", "utf8", (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            const pagesIndex = JSON.parse(data);
+            const searchIndex = lunr(function () {
+                if (!(pagesIndex && Array.isArray(pagesIndex) && pagesIndex.length > 0)) {
+                    return;
+                }
+
+                this.field("title");
+                this.field("content");
+                this.field("advisors");
+                this.field("students");
+                this.ref("href");
+
+                pagesIndex.forEach((page) => {
+                    const _page = {};
+                    Object.keys(page).forEach(key => {
+                        const obj = page[key]
+                        if (!(obj === null || obj === undefined)) {
+                            if (Array.isArray(obj)) {
+                                if (obj.length > 0) {
+                                    _page[key] = obj.map(function (t) {
+                                        return lunr.unicodeNormalizer(t);
+                                    }).join(" ");
+                                }
+                            }
+                            else {
+                                _page[key] = lunr.unicodeNormalizer(obj.toString());
+                            }
+                        }
+                    })
+                    this.add(_page)
+                });
+            });
+
+            fs.writeFile(CWD + '/static/searchIndex.json', JSON.stringify(searchIndex), (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+            });
+
+            fs.writeFile(CWD + '/static/pagesIndex.json', JSON.stringify(pagesIndex), (err) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+            });
+        });
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+initSearchIndex();
